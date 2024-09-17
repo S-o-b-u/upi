e75e0d35dd2297729162ff5b04113e82f37537f7
Souvik Rahut <souvikrahutofficial@gmail.com>
2024-09-17 18:04:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+var Stars = function(args) {
+    if (args === undefined) args = {};
+    var _scope = this;
+
+    this.stars = [];
+    this.vel = args.vel || 1;
+    this.radius = args.radius || 1;
+    this.alpha = 0.5;
+    this.starsCounter = args.stars || 300;
+    var center = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+    };
+    var canvas, context;
+    this.init = function() {
+        canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        context = canvas.getContext("2d");
+        context.lineCap = "round";
+        this.start();
+        this.resize();
+        // 
+        window.addEventListener("resize", this.resize.bind(this));
+    }
+
+    this.start = function() {
+        this.stars = [];
+        for (var i = 0; i < this.starsCounter; i++) {
+          setTimeout(function(){
+            _scope.stars.push(new Star());
+          }, i*30);
+        }
+    }
+
+    this.resize = function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        center.x = canvas.width / 2;
+        center.y = canvas.height / 2;
+    }
+
+    this.animate = function() {
+        window.requestAnimationFrame(this.animate.bind(this));
+        this.render();
+    }
+    this.render = function() {
+        context.fillStyle = 'rgba(17, 17, 17, 1)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.strokeStyle = "white";
+        for (var i = 0; i < this.stars.length; i++) this.stars[i].update();
+    }
+
+    var Star = function() {
+        this.x = center.x;
+        this.y = center.y;
+        this.init = function() {
+            this.radius = Math.random() * _scope.radius;
+            this.x = center.x;
+            this.y = center.y;
+            this.lineWidth = 0;
+            this.vel = {
+                x: Math.random() * 10 - 5,
+                y: Math.random() * 10 - 5
+            }
+        }
+        this.update = function() {
+            this.vel.x *= 1.05;
+            this.vel.y *= 1.05;
+            this.lineWidth += 0.035;
+            this.x0 = this.x;
+            this.y0 = this.y;
+            this.x += this.vel.x;
+            this.y += this.vel.y;
+            this.draw();
+            if (this.isDead()) this.init();
+        }
+        this.draw = function() {
+            context.beginPath();
+            context.moveTo(this.x0, this.y0);
+            context.lineTo(this.x, this.y);
+            context.lineWidth = this.lineWidth;
+            context.stroke();
+        }
+        this.isDead = function() {
+            return (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height);
+        }
+        this.init();
+        return this;
+    }
+    this.init();
+    this.animate();
+    return this;
+}
+
+var _stars = new Stars();
+
+
+// script.js
+document.querySelector('#payButton').addEventListener('click', function() {
+    const upiUrl = 'https://link.upilink.in/8348686983@yapl';
+
+    // Function to detect if the user is on a mobile device
+    function isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
+    if (isMobile()) {
+        // Redirect to UPI URL if on a mobile device
+        window.location.href = upiUrl;
+    } else {
+        // Provide fallback for desktop users
+        alert('Please scan the QR code using your UPI app on your mobile device to make the payment.');
+    }
+});
+
+document.getElementById('qrCodeDiv').addEventListener('click', function() {
+    const qrImage = document.getElementById('qrImage').src;
+    const link = document.createElement('a');
+    link.href = qrImage;
+    link.download = 'upi-qr-code.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
